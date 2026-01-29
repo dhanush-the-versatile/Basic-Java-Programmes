caba604f119845f9bce498c8a57460b13cc3c7e3
Amara Dhanush Kumar <amaradhanushkumar@gmail.com>
2026-01-29 09:47:53 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,18 @@
+class Registration
+{
+    public static void main(String[] args) 
+    {
+        String Fullname="Amara Dhanush Kumar";
+        char gender='M';
+        int age=20;
+        double height=5.8;
+        long phoneNumber=9492895382L;
+        boolean isMarried=false;
+        System.out.println("Full Name: "+Fullname);
+        System.out.println("Gender: "+gender);
+        System.out.println("Age: "+age);
+        System.out.println("Height: "+height);
+        System.out.println("Phone Number: "+phoneNumber);
+        System.out.println("Is Married: "+isMarried);
+    }
+}
